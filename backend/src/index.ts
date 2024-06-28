53979d2511b50c510e5a1f3c4cb4a090d94ea807
PachaiyappanV <pachaiyappan394@gmail.com>
2024-06-28 15:57:33 +0530

    feat(auth): implement user signup functionality with JWT generation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,37 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
 // Create the main Hono app
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-app.post("/api/v1/signup", (c) => {
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  console.log(prisma);
-  return c.text(`signup route${prisma}`);
+
+  try {
+    const body = await c.req.json();
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const jwt = await sign({ id: (await user).id }, c.env.JWT_SECRET);
+
+    return c.json({ jwt });
+  } catch (err) {
+    console.log(err);
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
 });
 
 app.post("/api/v1/signin", (c) => {
